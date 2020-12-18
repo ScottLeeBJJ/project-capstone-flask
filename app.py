cd32d7e039bb3834af2c098d003bfc79dd43aaef
ScottLeeBJJ <scottlee5455@outlook.com>
2020-12-18 14:59:19 -0800

    updated for games

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class Guide(db.Model):
+class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
     content = db.Column(db.String(144), unique=False)
@@ -20,42 +20,72 @@ class Guide(db.Model):
         self.content = content
 
 
-class GuideSchema(ma.Schema):
+class GameSchema(ma.Schema):
     class Meta:
         fields = ('title', 'content')
 
 
-guide_schema = GuideSchema()
-guides_schema = GuideSchema(many=True)
+game_schema = GameSchema()
+games_schema = GameSchema(many=True)
 
 # Endpoint to create a new guide
-@app.route('/guide', methods=["POST"])
+@app.route('/games', methods=["POST"])
 def add_guide():
     title = request.json['title']
     content = request.json['content']
 
-    new_guide = Guide(title, content)
+    new_game = Game(title, content)
 
-    db.session.add(new_guide)
+    db.session.add(new_game)
     db.session.commit()
 
-    guide = Guide.query.get(new_guide.id)
+    game = Game.query.get(new_game.id)
 
-    return guide_schema.jsonify(guide)
+    return game_schema.jsonify(game)
 
 
-# Enpoint to query all guides
-@app.route("/guides", methods=["GET"])
-def get_guides():
-    all_guides = Guide.query.all()
-    result = guides_schema.dump(all_guides)
+# Endpoint to query all games
+@app.route("/games", methods=["GET"])
+def get_games():
+    all_games = Game.query.all()
+    result = games_schema.dump(all_games)
     return jsonify(result)
 
-# Endpoint for querying a single guide
-@app.route("/guide/<id>", methods=["GET"])
-def get_guide(id):
-    guide = Guide.query.get(id)
-    return guide_schema.jsonify(guide)
+
+# Endpoint for querying a single game
+@app.route("/game/<id>", methods=["GET"])
+def get_game(id):
+    game = Game.query.get(id)
+    return game_schema.jsonify(game)
+
+
+# Endpoint for updating a game
+@app.route("/game/<id>", methods=["PUT"])
+def game_update(id):
+    game = Game.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    game.title = title
+    game.content = content
+
+    db.session.commit()
+    return game_schema.jsonify(game)
+
+
+# Endpoint for deleting a record
+@app.route("/game/<id>", methods=["DELETE"])
+def game_delete(id):
+    game = Game.query.get(id)
+    db.session.delete(game)
+    db.session.commit()
+
+    return "Game was successfully deleted"
+
+@app.route("/")
+def hello():
+    return("Hey Flask")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
